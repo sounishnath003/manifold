efd9084e2c331f63a864734bf028673b0f2b453d
Sounish Nath <flock.sinasini@gmail.com>
2023-07-29 22:59:35 +0530

    setup docker-environment for development

--- manifold-backend/src/app.service.ts
+++ manifold-backend/src/app.service.ts
@@ -1,8 +1,23 @@
 import { Injectable } from '@nestjs/common';
+import * as os from 'node:os';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  healthCheck() {
+    return {
+      message: '[PONGG]: I am alive',
+      information: {
+        hostname: os.hostname(),
+        totalCpu: os.cpus().length,
+        architecture: os.arch(),
+        freeMemory: os.freemem(),
+        avgLoad: os.loadavg(),
+        machine: os.machine(),
+        platform: os.platform(),
+        networkInfo: os.networkInterfaces(),
+        uptime: os.uptime(),
+        release: os.release(),
+      },
+    };
   }
 }
